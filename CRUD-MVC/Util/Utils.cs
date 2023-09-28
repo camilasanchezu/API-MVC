19c77c4fc0a91870397d9c4ef0828290fa47d2b2
MateoRosero <mateorgafjo@gmail.com>
2023-09-27 21:03:23 -0500

    Agregar archivos de proyecto.

---
+++ CRUD-MVC/Util/Utils.cs
@@ -0,0 +1,34 @@
+﻿using CRUD_MVC.Models;
+
+namespace CRUD_MVC.Util
+{
+    public class Utils
+    {
+        public static List<Producto> ListaProducto = new List<Producto>()
+        {
+            new Producto()
+            {
+                IdProducto = 1,
+                Nombre = "Harina",
+                Descripcion = "Marca Patito",
+                Cantidad = 20
+            },
+
+            new Producto()
+            {
+                IdProducto = 2,
+                Nombre = "Cepillo",
+                Descripcion = "Marca Patito",
+                Cantidad = 15
+            },
+
+            new Producto()
+            {
+                IdProducto = 3,
+                Nombre = "TV",
+                Descripcion = "Marca Sony",
+                Cantidad = 2
+            }
+        };
+    }
+}
